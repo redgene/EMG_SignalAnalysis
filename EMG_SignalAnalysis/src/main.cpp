4ebb47082f20bc797fa6f122e597ae95a4411215
Utkarsh Tripathi <f2016894@pilani.bits-pilani.ac.in>
2020-01-12 12:40:50 +0530

    update

--- EMG_SignalAnalysis/src/main.cpp
+++ EMG_SignalAnalysis/src/main.cpp
@@ -1,9 +1,7 @@
 #include <Arduino.h>
-
 void setup() {
-  // put your setup code here, to run once:
-}
-
+  Serial.begin(9600);
+  }
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println(analogRead(A0));
 }
